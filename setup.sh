384a1d2bdc2428cc56d819cca21fc8a2e28fbe46
Nic Smith <nicholas.j.smith-2@student.uts.edu.au>
2025-01-31 01:31:25 +0000

    install nav2
    Signed-off-by: Nic Smith <nicholas.j.smith-2@student.uts.edu.au>

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ROS_DISTRO=humble
+source /opt/ros/$ROS_DISTRO/setup.bash
+sudo apt install -y \
+  ros-$ROS_DISTRO-navigation2 \
+  ros-$ROS_DISTRO-nav2-bringup \
+  ros-$ROS_DISTRO-turtlebot3*
